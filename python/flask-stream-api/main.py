d8d2151c6f93eb178014967aa359c32d4fea8109
Gerhardt Datsomor <datsomorg@gmail.com>
2023-10-25 09:48:05 +0000

    sample flask chat steaming api

---
+++ python/flask-stream-api/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, Response
+import time 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def chat_events():
+    # Implement your chat logic here
+    # For example, you can have a list of messages and yield them one by one.
+    messages = ["Hello", "How are you?", "I'm fine, thanks!"]
+    for message in messages:
+        yield f"data: {message}\n\n"
+        time.sleep(1)
+
+@app.route('/stream_chat')
+def stream_chat():
+    return Response(chat_events(), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
