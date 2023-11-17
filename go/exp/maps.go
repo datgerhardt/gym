579cb29cd39493b61b8eddfd2bc967c400db5677
Gerhardt Datsomor <datsomorg@gmail.com>
2023-11-17 10:16:25 +0000

    restructure

---
+++ go/exp/maps.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// var nilMap map[string]int
+	totalWins := map[string]int{}
+
+	// teams := map[string][]string {
+	// 	"orca": []string{"Fred", "Raplh"},
+	// }
+	totalWins["Orcas"] = 1
+	totalWins["Loins"] = 2
+
+}
