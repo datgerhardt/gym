da5690120029b851e2fb7ebaddd1fdf53234e0a0
Gerhardt Datsomor <datsomorg@gmail.com>
2025-06-21 11:51:25 +0000

    implement cat file

--- python/wyag/libwyag.py
+++ python/wyag/libwyag.py
@@ -1,7 +1,8 @@
 import argparse
+from bz2 import compress
 import configparser
 from datetime import datetime
-from nt import write
+from nt import mkdir, write
 from ntpath import isdir
 import grp, pwd
 
@@ -11,6 +12,8 @@ from math import ceil
 import os
 import re
 import sys
+from typing import override
+from unittest import result
 import zlib
 
 
@@ -174,3 +177,113 @@ def repo_find(path=".", required=True):
 
     # Recursive case
     return repo_find(parent, required)
+
+class GitObject(object):
+
+    def __init__(self, data=None) -> None:
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+    
+    def serialize(self, repo):
+        """This function MUST be implemented by subclasses.
+
+        It must read the object's contents from self.data, a byte string, and
+        do whatever it takes to convert it into a meaningful representation.
+        What exactly that means depend on each subclass.
+
+        """
+        raise Exception("Unimplemented")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented")
+    
+    def init(self):
+        pass
+
+        
+def object_read(repo, sha):
+    """Read object sha from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    if not os.path.isfile(path):
+        return  None
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type 
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size 
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception(f"Malformed object {sha}: bad length")
+
+        # pick constructor
+        match fmt:
+            case b'commit' : c=GitCommit
+            case b'tree' : c=GitTree
+            case b'tag' : c=GitTag
+            case b'blob' : c=GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode('ascii')} for object {sha}")
+
+    # call constructor and return object
+    return c(raw[y+1:])
+
+
+def object_write(obj, repo=None):
+    # Serialize object data
+    data = obj.serialize()
+    # Add header
+    result = obj.fmt + b' ' + str(len(data)).encode() +  b'\x00' + data
+    # Compute jhash 
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        # Compute path
+        path=repo_path(repo, 'objects', sha[0:2], sha[2:], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                # compress and write
+                f.write(zlib,compress(result))
+
+    return sha
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+
+# wyag cat-file TYPE OBJECT
+
+argsp = argsubparsers.add_parser( "cat-file", 
+                                    help="Provide content of repository objects.")
+argsp.add_argument("type", metavar=["blob", "commit", "tag", "tree"], help="Specify the type")
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+    
+def object_find(repo, name, fmt=None, fellow=True):
+    return name
+
+
+# wyag hash-object [-w] [-t TYPE] FILE
